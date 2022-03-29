c0ea61187c4e43f2485196f2a5556e8fd2de51ff
Terry MacDonald <terry.macdonald@cosive.com>
2022-03-30 09:33:37 +1300

    [WIP] Working mostly
    Need to fix

--- NVIDIAInfo/Program.cs
+++ NVIDIAInfo/Program.cs
@@ -53,10 +53,10 @@ namespace NVIDIAInfo
             NLog.LogManager.Configuration = config;
 
             // Start the Log file
-            SharedLogger.logger.Info($"NVIDIAInfo/Main: Starting NVIDIAInfo v1.7.0");
+            SharedLogger.logger.Info($"NVIDIAInfo/Main: Starting NVIDIAInfo v1.7.1");
 
 
-            Console.WriteLine($"\nNVIDIAInfo v1.7.0");
+            Console.WriteLine($"\nNVIDIAInfo v1.7.1");
             Console.WriteLine($"=================");
             Console.WriteLine($"By Terry MacDonald 2022\n");
 
