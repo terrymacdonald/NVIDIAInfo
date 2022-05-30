3cb80315d2f4be4d8a263b8f497e458a41f437ee
Terry MacDonald <terry.macdonald@cosive.com>
2022-05-31 07:44:13 +1200

    Updated to v1.7.5

--- NVIDIAInfo/Program.cs
+++ NVIDIAInfo/Program.cs
@@ -53,10 +53,10 @@ namespace NVIDIAInfo
             NLog.LogManager.Configuration = config;
 
             // Start the Log file
-            SharedLogger.logger.Info($"NVIDIAInfo/Main: Starting NVIDIAInfo v1.7.4b");
+            SharedLogger.logger.Info($"NVIDIAInfo/Main: Starting NVIDIAInfo v1.7.5");
 
 
-            Console.WriteLine($"\nNVIDIAInfo v1.7.4b");
+            Console.WriteLine($"\nNVIDIAInfo v1.7.5");
             Console.WriteLine($"=================");
             Console.WriteLine($"By Terry MacDonald (c) 2022\n");
 
